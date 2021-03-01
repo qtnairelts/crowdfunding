e84ada77ec7f4f95b079cb3dda14dbea5ab5d061
shandy <qtnairelts@yahoo.com>
2021-03-01 21:01:30 +0800

    two continue botton dead-.-""

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-let stock = document.querySelector(".stock");
-let mah = document.getElementById('mah')
-let mahselect = document.getElementById('mahSelectButton')
-let mahPledge = document.getElementById('mahPledge')
+
+
 let myCheckbox = document.getElementById("toggle");
 let dim = document.getElementById("dim");
 // let style = getComputedStyle(bam)
+let card = document.getElementsByName('card')
+let enter = document.getElementsByName("enter")
 
 
 
@@ -16,19 +16,28 @@ const Model = [
         radio: "radiolab0",
         card: "card1",
         enter: "enter1",
+        stocks: "",
+
     },
     {
+        tier: "Tier1",
         radio: "radiolab1",
         card: "card2",
         enter: "enter2",
+        stocks: "101",
     }, {
+        tier: "Tier2",
         radio: "radiolab2",
         card: "card3",
         enter: "enter3",
+        stocks: "64",
+
     }, {
+        tier: "Tier3",
         radio: "radiolab3",
         card: "card4",
         enter: "enter4",
+        stocks: "0",
     },
 ];
 
@@ -57,48 +66,75 @@ for (var i = 0, length = radios.length; i < length; i++) {
         // console.log("getcard", getcard)
         // console.log("getenter:", getenter)
         //////////////////change-border-color&prop-out-enter///////////////////
-        let card = document.getElementsByName('card')
-        let enter = document.getElementsByName("enter") 
-        for (var i = 0; i <= card.length; i++) {
+
+        for (var i = 0; i < card.length; i++) {
             for (var j = 0; j < enter.length; j++) {
-               
-            // console.log("card::",card[i].id)
-            if (card[i].id !== getcard && enter[j].id !== getenter) {
-                console.log("enter::",enter[j].id)
-                card[i].style.borderColor = "hsl(210, 6%, 94%)" 
-                enter[j].style.display = "none";
-            } else if (card[i].id == getcard && enter[j].id == getenter ) {
-                
-                // console.log("aimcard::",card[i].id)
-                // console.log("enteraim::",enter[j].id)
-                card[i].style.borderColor = "hsl(176, 50%, 47%)";
-                enter[j].style.display = "grid";     
-            } 
-         }
+
+                // console.log("card::",card.length)
+                // console.log("enter::",enter.length)
+                if (card[i].id !== getcard && enter[j].id !== getenter) {
+                    // console.log("enter::", enter[j].id)
+                    card[i].style.borderColor = "hsl(210, 6%, 94%)"
+                    enter[j].style.display = "none";
+                } else if (card[i].id == getcard && enter[j].id == getenter) {
+
+                    // console.log("aimcard::",card[i].id)
+                    // console.log("enteraim::",enter[j].id)
+                    card[i].style.borderColor = "hsl(176, 50%, 47%)";
+                    enter[j].style.display = "grid";
+                }
+            }
+          
+
         }
         ////////////////////////////////////////////////////////
         
+        
+
     }
 }
 
-// if (mahstock.innerHTML==0){
-//     mah.style.opacity = 0.5;
-//     mahselect.style.backgroundColor = "rgb(122, 122, 122)";
-//     mahPledge.style.color = "rgb(122, 122, 122)"
-// }
+///////////////gray out the card if out of stock//////////////
+let stock = document.getElementsByName('amount')
+let selectButton = document.getElementsByName('SelectButton')
+let Pledge = document.getElementsByName('pledge')
+
+for (var j =0 ; j < stock.length; j++) {
+    // console.log(selectButton[j])
+    // stock[j].textContent = Model[j + 1].stocks;
+    if (stock[j].innerHTML == 0) {
+        card[j].style.opacity = 0.5;
+        card[j].style.pointerEvents = 'none'
+        card[j+4].style.opacity = 0.5;
+        card[j+4].style.pointerEvents = 'none'
+        selectButton[j].style.backgroundColor = "rgb(122, 122, 122)";
+        Pledge[j].style.color = "rgb(122, 122, 122)"
+    }   
+}
+/////////////////////////////////////////////////////////
+//////////////continue & Gotit button////////////////
 let continues = document.getElementById("continues")
 let successsheet = document.getElementById("successs")
 let gotitbutton = document.getElementById("gotit")
-console.log(successsheet.style)
+let dollar = document.getElementById("value")
+let dollarvalue = parseFloat(dollar.innerHTML)
+let progressvalue = document.getElementById("progressbar").value
+
 continues.onclick = function () {
     backprojectsheet.style.zIndex = "-999"
-    successsheet.style.zIndex ="999";
+    successsheet.style.zIndex = "999";
     dimm();
 }
 gotitbutton.onclick = function () {
-    successsheet.style.zIndex ="-999";
+    dollarvalue += 1
+    dollar.innerHTML = dollarvalue
+    console.log(dollarvalue)
+    document.getElementById("progressbar").value = dollarvalue/100000*100
+    // console.log(document.getElementById("progressbar").value )
+    successsheet.style.zIndex = "-999";
     undim();
 }
+
 
 ///////////menu-open&close///////////////
 myCheckbox.oninput = function () {
@@ -129,7 +165,6 @@ backbutton.onclick = function () {
 }
 closebutton.onclick = function () {
     backprojectsheet.style.zIndex = "-999";
-    undim()
-    console.log(backprojectsheet.style.margintop)
+    undim();
 }
 /////////////////////////////////////////////////////
